f2095f01e6a42c9c9c7480b9363c41e789350615
dekim1028 <daeun235194@gmail.com>
2020-12-14 13:30:18 +0900

    Fix typo

--- src/container/content/widget/WeatherWidgetContainer.js
+++ src/container/content/widget/WeatherWidgetContainer.js
@@ -6,7 +6,7 @@ const WeatherWidgetContainer = ({thema}) => {
     const [weatherData,setWeatherData] = useState(null);
 
     const getWeather = () =>{
-        var apiURI = "http://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=435a348a1fb9bceec1c8fa96c34752a5";
+        var apiURI = "https://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=435a348a1fb9bceec1c8fa96c34752a5";
         axios({
             url: apiURI,
             dataType: "json",
